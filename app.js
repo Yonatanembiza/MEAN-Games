72e73f139b92ae640107918e06435cfba3378e18
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2024-07-27 09:35:28 -0500

    done

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const server= app.listen(process.env.PORT, function(){
 app.use(express.static(path.join(__dirname, "public")))
 
 // creat get request
-app.get("/index.html", function(req, res){
+app.get("/", function(req, res){
     console.log("Request is being served!");
     res.status(200).sendFile("index.html")
 });
